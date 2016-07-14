343e58d54bdfa103416283a563ab1712b41d7aa6
Chip Cullen <ccullen@pbs.org>
2016-07-14 14:42:07 -0400

    through video 4, got routes set up

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -9,5 +9,20 @@ import css from './styles/style.styl';
 // import components
 
 import Main from './components/Main.js';
+import Single from './components/Single.js';
+import PhotoGrid from './components/PhotoGrid.js';
 
-render(<Main />, document.getElementById('root'));
+// import react router deps
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+
+  </Router>
+)
+render(router, document.getElementById('root'));
